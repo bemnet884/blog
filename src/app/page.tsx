0412065ce33173a7efc4d748d9aeb9466bd9d97b
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-11-18 18:06:17 +0300

    header create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-blue-500 text-center text-5xl min-h-screen mx-auto flex items-center justify-center">Hello</h1>
+      <h1 className="font-bold text-blue-500 text-center text-5xl min-h-screen mx-auto flex items-center justify-center">Blog</h1>
     </>
   );
 }
